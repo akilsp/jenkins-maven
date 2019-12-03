f9440b30d98d059c818e4c1c4b16a59651ec27c8
PonAkilan Senthilvel <ponakilan.senthilvel@cgi.com>
2019-12-03 12:08:59 +0200

    Junit Test and dependency added

--- src/test/java/com/xyz/AppTest.java
+++ src/test/java/com/xyz/AppTest.java
@@ -10,7 +10,7 @@ public class AppTest {
     @Test
     public void testGetToday() {
         String actual = App.getToday();
-        String expected = new SimpleDateFormat("dd-MMM-yyyy").format(new Date());
+        String expected = new SimpleDateFormat("dd-mm-yyyy").format(new Date());
         assertEquals(expected, actual);
     }
 }
