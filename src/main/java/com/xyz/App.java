681bbb09acddfde69b163f90b252b5cb49014886
PonAkilan Senthilvel <ponakilan.senthilvel@cgi.com>
2019-12-03 10:44:12 +0200

    spell check fixed

--- src/main/java/com/xyz/App.java
+++ src/main/java/com/xyz/App.java
@@ -5,7 +5,7 @@ import org.apache.commons.lang3.time.DateFormatUtils;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello, Toaday is "+ getToday());
+		System.out.println("Hello, Today is "+ getToday());
 	}
 	public static String getToday() {
 		String today = DateFormatUtils.format(new Date(), "dd-MMM-yyyy");
