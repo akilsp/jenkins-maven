c1e8def554a76a0e823e8043406db1453a061fcb
PonAkilan Senthilvel <ponakilan.senthilvel@cgi.com>
2019-12-03 10:27:07 +0200

    more dependency added in pom file

--- src/main/java/com/xyz/App.java
+++ src/main/java/com/xyz/App.java
@@ -1,6 +1,14 @@
  package com.xyz;
+
+import java.utils.Date;
+import org.apache.commons.lang.time.DateFormatUtils;
+
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello, Toaday is "+ getToday());
+	}
+	public static String getToday() {
+		String today = DateFormatUtils.format(new Date(), "dd-MMM-yyyy");
+		return today;
 	}
 }
