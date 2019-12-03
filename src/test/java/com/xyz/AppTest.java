8df1f5ad3b6acf0a2c1315cc674748bd6e10b606
PonAkilan Senthilvel <ponakilan.senthilvel@cgi.com>
2019-12-03 12:04:33 +0200

    Junit Test and dependency added

---
+++ src/test/java/com/xyz/AppTest.java
@@ -0,0 +1,16 @@
+package com.xyz;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testGetToday() {
+        String actual = App.getToday();
+        String expected = new SimpleDateFormat("dd-MMM-yyyy").format(new Date());
+        assertEquals(expected, actual);
+    }
+}
